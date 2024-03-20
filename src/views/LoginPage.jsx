d439a8023da23bc99ff072d4aff6df9dfa719a5d
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-03-20 23:06:26 +0300

    Made functionality to allow user create an account on our app using their email and password through firebase

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,13 +1,40 @@
 import FullPageLoader from "../components/FullPageLoader.jsx";
 import { useState } from "react";
 import { auth } from "../firebase/config.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [loginType, setLoginType] = useState("login");
+  const [userCredentials, setUserCredentials] = useState({});
 
-  console.log(auth);
+  const handleCredentials = (e) => {
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
+    console.log(userCredentials);
+  };
 
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(
+      auth,
+      userCredentials.email,
+      userCredentials.password
+    )
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ..
+      });
+  };
   return (
     <>
       {isLoading && <FullPageLoader></FullPageLoader>}
@@ -33,11 +60,17 @@ function LoginPage() {
           <form className="add-form login">
             <div className="form-control">
               <label>Email *</label>
-              <input type="text" name="email" placeholder="Enter your email" />
+              <input
+                onChange={(e) => handleCredentials(e)}
+                type="text"
+                name="email"
+                placeholder="Enter your email"
+              />
             </div>
             <div className="form-control">
               <label>Password *</label>
               <input
+                onChange={(e) => handleCredentials(e)}
                 type="password"
                 name="password"
                 placeholder="Enter your password"
@@ -46,7 +79,14 @@ function LoginPage() {
             {loginType == "login" ? (
               <button className="active btn btn-block">Login</button>
             ) : (
-              <button className="active btn btn-block">Sign Up</button>
+              <button
+                onClick={(e) => {
+                  handleSignUp(e);
+                }}
+                className="active btn btn-block"
+              >
+                Sign Up
+              </button>
             )}
 
             <p className="forgot-password">Forgot Password?</p>
