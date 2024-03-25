d5dd7d930bcd66950a33b6507961e84aeeccb92f
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-03-25 16:17:05 +0300

    Implemented the sign out function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,44 @@
 import { NavLink } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/usersSlice.js";
 
-function Header({pageTitle}) {
+function Header({ pageTitle }) {
+  const dispatch = useDispatch();
 
-    return (
-      <>
+  const handleSignOut = () => {
+    if (confirm("Are you sure you want to log out?")) {
+      signOut(auth)
+        .then(() => {
+          // On successful sign out
+          dispatch(setUser(null));
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  };
 
-            <h1>{pageTitle}</h1>
+  return (
+    <>
+      <h1>{pageTitle}</h1>
 
-            <div className="header-btns">
+      <div className="header-btns">
+        <NavLink to="/">
+          <button className="btn">Books</button>
+        </NavLink>
 
-                    <NavLink to="/">
-                      <button className="btn">
-                          Books
-                      </button>
-                    </NavLink>
+        <NavLink to="/add-book">
+          <button className="btn">Add Book +</button>
+        </NavLink>
 
-                    <NavLink to="/add-book">
-                      <button className="btn">
-                          Add Book +
-                      </button>
-                    </NavLink>
+        <button onClick={handleSignOut} className="btn transparent">
+          Logout
+        </button>
+      </div>
+    </>
+  );
+}
 
-                    <button className="btn transparent">
-                      Logout
-                    </button>
-
-               
-            </div>
-    
-      </>
-    )
-  }
-  
-  export default Header
-  
+export default Header;
